0c2f6ea487429f78a87922a6f9a8da547555342f
Ricardo Rojas <ricardorojas320@aragon.unam.mx>
2025-08-27 17:01:10 -0600

    Operaciones

---
+++ Tarea01/app.js
@@ -0,0 +1,20 @@
+
+let numero1 = parseFloat(prompt("Ingresa primer número"));
+let numero2 = parseFloat(prompt("Ingresa segundo número"));
+
+// Operaciones
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multi = numero1 * numero2;
+let div = numero1 / numero2;
+let modulo = numero1 % numero2;
+
+console.log("Resultado de suma es de : " + suma);
+
+console.log("Resultado de resta es de : " + resta);
+
+console.log("Resultado de multiplicacion es de : " + multi);
+
+console.log("Resultado de division es de : " + div);
+
+console.log("Resultado de modulo es de : " + modulo);
